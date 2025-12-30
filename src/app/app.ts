fbecf118f6192567abb212642f995a1f5c48e3f1
Mykola Skorenkyi <mykola.skorenky@gmail.com>
2025-12-31 01:30:24 +0200

    suppress scss @import warnings

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('my-app-name');
+  protected readonly currentYear = signal(new Date().getFullYear());
 }
